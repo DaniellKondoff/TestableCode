251fdf5a4f90900be7bf1221a1adcb31413d1334
Daniel Kondow <danielkondov91@gmail.com>
2020-02-20 23:29:34 +0200

    Framework

---
+++ Blog.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Blog.Controllers;
+using Blog.Controllers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Xunit;
+
+namespace Blog.Test.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void AboutShouldReturnViewResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.About();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void PrivacyShouldReturnViewResultWIthCorrectUserName()
+        {
+            const string UserName = "TestUser";
+            var homeController = new HomeController();
+            homeController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, UserName)
+                    }))
+                }
+            };
+
+            var result = homeController.Privacy();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<PrivacyViewModel>(viewResult.Model);
+            Assert.Equal(UserName, model.Username);
+        }
+    }
+}
