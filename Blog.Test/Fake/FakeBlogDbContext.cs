9838fe2a08d20742edfc964d6a016c28c37d647f
Daniel Kondov <daniel.kondov@icb.bg>
2020-03-04 18:22:10 +0200

    Database Testing

---
+++ Blog.Test/Fake/FakeBlogDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Blog.Test.Fake
+{
+    public class FakeBlogDbContext
+    {
+        
+        public FakeBlogDbContext(string name)
+        {
+            var options = new DbContextOptionsBuilder<BlogDbContext>()
+                .UseInMemoryDatabase(name)
+                .Options;
+
+           this.Data = new BlogDbContext(options);
+        }
+
+        public BlogDbContext Data { get; }
+
+        public async Task Add(params object[] data)
+        {
+            this.Data.AddRange(data);
+            await this.Data.SaveChangesAsync();
+        }
+    }
+}
---
+++ Blog.Test/Services/ArticleServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using Blog.Data.Models;
+using Blog.Services;
+using Blog.Test.Fake;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blog.Test.Services
+{
+    public class ArticleServiceTest
+    {
+        [Fact]
+        public async Task IsByUserShouldReturnTrueWhenArticleByTheSpecificUserExists()
+        {
+            //Arenge
+            const string databaseName = "ArticlesIsByUser";
+
+            var acticleService = await this.GetArticleService(databaseName);
+
+            //Act
+            var exist = await acticleService.IsByUser(1, "1");
+
+            //Assert
+            Assert.True(exist);
+        }
+
+        [Fact]
+        public async Task IsByUserShouldReturnFalseWhenArticleByTheSpecificUserDoesNotExists()
+        {
+            //Arenge
+            const string databaseName = "ArticlesIsByUserDoesNotExists";
+           
+            var acticleService = await this.GetArticleService(databaseName);
+
+            //Act
+            var exist = await acticleService.IsByUser(3, "1");
+
+            //Assert
+            Assert.False(exist);
+        }
+
+        private async Task AddFakeArticles(FakeBlogDbContext fakeDb)
+        {
+            await fakeDb.Add(new Article
+            {
+                Id = 1,
+                UserId = "1",
+                Title = "Test Article"
+            },
+            new Article
+            {
+                Id = 2,
+                UserId = "2",
+                Title = "Test Article 2"
+            });
+        }
+
+        private async Task<ArticleService> GetArticleService(string databaseName)
+        {
+            var db = new FakeBlogDbContext(databaseName);
+
+            await AddFakeArticles(db);
+
+            return new ArticleService(db.Data);
+        }
+    }
+}
